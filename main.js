1bdfd5ba9f82e9238344248d3def2e1dedc1ef72
Jared Gleisner <darkphoton31@gmail.com>
2024-11-17 23:35:57 -0600

    Established base model

---
+++ main.js
@@ -0,0 +1,21 @@
+confetti = function() {
+    // spawn confetti shapes that fall from the top of the screen to the bottom
+    for (var i = 0; i < 200; i++) {
+        const confetti = document.createElement("div")
+        confetti.className = "confetti";
+        confetti.style.left = Math.random() * window.innerWidth + "px";
+        confetti.style.animationDuration = Math.random() * 5 + 5 + "s";
+        confetti.style.animationDelay = Math.random() * 5 + "s";
+        confetti.style.backgroundColor = "hsl(" + Math.random() * 360 + ", 100%, 50%)";
+        slide = Math.random() * 100;
+        confetti.style.setProperty("--left", slide+"%");
+        document.body.appendChild(confetti);
+
+        //the falling animation
+        confetti.addEventListener("animationend", function() {
+            this.remove();
+        });
+        
+        
+    }
+}
